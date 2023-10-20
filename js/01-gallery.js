b062605a7bcfc167102dd35e042ff9c84cb67a30
Baltakov <tira2500@gmail.com>
2023-10-20 07:43:42 +0300

    Task 1 comments

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,12 @@
 import { galleryItems } from "./gallery-items.js";
 
+// Получаем элемент .gallery в DOM.
 const gallery = document.querySelector(".gallery");
+// Получаеv массив всех элементов .gallery__image в DOM.
 const galleryImages = document.querySelectorAll(".gallery__image");
 
+// Функция createMarkup() принимает массив элементов галереи в качестве
+// входных данных и возвращает строку, содержащую HTML - разметку для галереи.
 function createMarkup(galleryItems) {
   return galleryItems
     .map(
@@ -23,23 +27,30 @@ function createMarkup(galleryItems) {
 
 gallery.innerHTML = createMarkup(galleryItems);
 
+// Добавляем обработчик события "click" к элементу .gallery
 gallery.addEventListener("click", openOriginalIMG);
+// modalOpen отслеживает, открыто ли модальное окно
 let modalOpen = false;
+// instance хранит ссылку на объект модального окна
 let instance;
+//функция openOriginalIMG() принимает объект события щелчка в качестве
+//входных данных и открывает модальное окно с оригинальным изображением в нем.
 function openOriginalIMG(e) {
   e.preventDefault();
-
+  //получаем ссылку на элемент .gallery__link, на который нажал пользователь.
   const imgOriginal = e.target.closest(".gallery__link");
-
+  //создаем новый экземпляр объекта модального окна basicLightbox
   instance = basicLightbox.create(`
     <img src="${imgOriginal.getAttribute("href")}" width="800" height="600">
   `);
 
   instance.show();
   modalOpen = true;
+  //добавляем обработчик события "keydown" к документу.
   document.addEventListener("keydown", closeModal);
 }
-
+//функци closeModal() принимает объект события клавиатуры в качестве
+//входных данных и закрывает модальное окно.
 function closeModal(event) {
   if (event.key === "Escape" && modalOpen) {
     instance.close();
